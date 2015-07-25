609363e998d04755e643838b0c5ef91f2ae44e30
wenbo <yiyun6674@hotmail.com>
2015-07-25 19:02:01 +0800

    now logout works: /login.php?action=logout

--- login.php
+++ login.php
@@ -6,4 +6,13 @@
 	 <input type="submit" name="submit" value="登陆" />
 	 &nbsp;&nbsp;&nbsp;&nbsp;
 <a href="register.php">注册</a>
-											 </form>
+</form>
+<?php
+//注销登录
+if($_GET['action'] == "logout"){
+	session_start();
+	unset($_SESSION['userid']);
+	unset($_SESSION['username']);
+	exit;
+}
+?>
